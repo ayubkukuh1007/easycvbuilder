a9115dec67186ea5708c9c60d2c71b3fe2466043
ayubkukuh1007 <ayubkukuhprasetyo434@gmail.com>
2023-01-10 11:58:55 +0800

    Implementasi halaman Experience

---
+++ app/src/main/java/com/application/easycvbuilder/Experience.kt
@@ -0,0 +1,11 @@
+package com.application.easycvbuilder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Experience : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_experience)
+    }
+}
