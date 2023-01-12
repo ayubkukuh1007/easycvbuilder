90ba06a971826f54ae7d8ea2da1e249e7d6582fe
ayubkukuh1007 <ayubkukuhprasetyo434@gmail.com>
2023-01-13 00:31:36 +0800

    implementasi design halaman form social media easycvbuilder

---
+++ app/src/main/java/com/application/easycvbuilder/Socialmedia.kt
@@ -0,0 +1,11 @@
+package com.application.easycvbuilder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Socialmedia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sosial_media)
+    }
+}
