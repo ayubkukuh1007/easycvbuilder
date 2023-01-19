d237ae9e376fae321a5329cd6b272e5b73feb315
ayubkukuh1007 <ayubkukuhprasetyo434@gmail.com>
2023-01-20 00:25:57 +0800

    implementasi design layout template 2 easycvbuilder

---
+++ app/src/main/java/com/application/easycvbuilder/f_tem_fragment/Fragment_template_two.kt
@@ -0,0 +1,12 @@
+package com.application.easycvbuilder.f_tem_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.application.easycvbuilder.R
+
+class Fragment_template_two : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment_template_two)
+    }
+}
